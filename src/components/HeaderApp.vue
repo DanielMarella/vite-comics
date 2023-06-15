2b35eb973cb742fd2a36230c7c878a0baedf268d
DanielMarella <marella.daniel96@gmail.com>
2023-06-15 15:52:57 +0200

    creating the header structure

--- src/components/HeaderApp.vue
+++ src/components/HeaderApp.vue
@@ -1,6 +1,26 @@
 <template>
     <header>
-        <h1>Header</h1>
+        <div class="header-container">
+            <div class="head-logo">
+                <ul>
+                    <li>Logo</li>
+                </ul>
+            </div>
+            <div class="head-nav">
+                <ul>
+                    <li>Link</li>
+                    <li>Link</li>
+                    <li>Link</li>
+                    <li>Link</li>
+                    <li>Link</li>
+                    <li>Link</li>
+                    <li>Link</li>
+                    <li>Link</li>
+                    <li>Link</li>
+                    <li>Link</li>
+                </ul>
+            </div>
+        </div>
     </header>
 </template>
 
@@ -10,5 +30,35 @@ export default {
 }
 </script>
 <style lang="scss">
-    
+
+    header{
+        position: relative;
+        background-color: #202020;
+        height: 150px;
+    }
+
+    .header-container{
+        width: 100%;
+        padding: 3rem 0;
+        position: relative;
+        top: 30%;
+        background-color: #fff;
+        display: flex;
+        justify-content: space-between;
+    }
+
+
+    .head-nav{
+            ul{
+                display: flex;
+            }
+
+            li{
+                margin: 0 10px;
+            }
+        }
+
+
+
+
 </style>
