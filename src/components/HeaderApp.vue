c2cf63e150a4120f9eda74506101abd9f1027046
DanielMarella <marella.daniel96@gmail.com>
2023-06-15 17:56:14 +0200

    add links in the header dinamically

--- src/components/HeaderApp.vue
+++ src/components/HeaderApp.vue
@@ -2,20 +2,16 @@
     <header>
         <div class="header-container">
             <div class="head-logo">
+                <img :src="getImagePath(`../assets/img/${imageLogo}`)" alt="">
             </div>
-            <img :src="getImagePath(`../assets/img/${imageLogo}`)" alt="">
             <div class="head-nav">
                 <ul>
-                    <li>Link</li>
-                    <li>Link</li>
-                    <li>Link</li>
-                    <li>Link</li>
-                    <li>Link</li>
-                    <li>Link</li>
-                    <li>Link</li>
-                    <li>Link</li>
-                    <li>Link</li>
-                    <li>Link</li>
+                    <li v-for="link in linkHeader" :class=" link.active ? 'active' : ''">
+                        <a :href="link.link" >
+                            {{ link.name }}
+                        </a>
+                        
+                    </li>
                 </ul>
             </div>
         </div>
@@ -27,7 +23,62 @@ export default {
     name : 'HeaderApp',
     data(){
         return{
-            imageLogo : "dc-logo.png"
+            imageLogo : "dc-logo.png",
+
+            linkHeader : [
+                {
+                    link : '#',
+                    name: 'characters',
+                    active : false
+                },
+                {
+                    link : '#',
+                    name: 'comics',
+                    active : true
+                },
+                {
+                    link : '#',
+                    name: 'movies',
+                    active : false
+                },
+                {
+                    link : '#',
+                    name: 'tv',
+                    active : false
+                },
+                {
+                    link : '#',
+                    name: 'games',
+                    active : false
+                },
+                {
+                    link : '#',
+                    name: 'collectibles',
+                    active : false
+                },
+                {
+                    link : '#',
+                    name: 'videos',
+                    active : false
+                },
+                {
+                    link : '#',
+                    name: 'fans',
+                    active : false
+                },
+                {
+                    link : '#',
+                    name: 'news',
+                    active : false
+                },
+                {
+                    link : '#',
+                    name: 'shop',
+                    active : false
+                },
+                
+            ]
+
         }
 
         
@@ -44,30 +95,45 @@ export default {
 <style lang="scss">
 
     .header-container{
-        width: 100%;
         height: 10vh;
         margin-top: 30px;
         background-color: #fff;
         display: flex;
-        align-items: center;
         justify-content: space-between;
     }
 
+
     img{
         width: 80px;
+        margin-left: 0;
     }
+
+    .head-logo{
+        display: flex;
+        align-items: center;
+        margin-left: 150px;
+    }
+
+    .head-nav{
+        margin-right: 150px;
+    }
+
 
     .head-nav{
             ul{
                 display: flex;
+                align-items: center;
+                height: 100%;
+                text-transform: uppercase;
             }
 
             li{
+                display: flex;
+                align-items: center;
+                height: 100%;
                 margin: 0 10px;
             }
         }
-
-
 
 
 </style>
