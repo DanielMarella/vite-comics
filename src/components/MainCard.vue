09a31ab8cebfe294924876d4cbf8a2c8b9d7a8ee
DanielMarella <marella.daniel96@gmail.com>
2023-06-16 18:05:55 +0200

    add style to the card

--- src/components/MainCard.vue
+++ src/components/MainCard.vue
@@ -1,6 +1,6 @@
 
 <template>
-    <div>
+    <div class="card">
         <img :src="cardElement.thumb" alt="">
         <p>
             {{ cardElement.series }}
@@ -20,5 +20,21 @@ export default {
 
 
 <style lang="scss" scoped>
-    
+    .card{
+        width : calc( (100% / 6) - 40px);
+        margin: 0 20px;
+        margin-top: 30px;
+
+        img{
+            width: 150px;
+            height: 150px;
+            object-fit: cover;
+        }
+
+        p{
+            margin-top: 10px;
+            font-size: .8rem;
+            text-transform: uppercase;
+        }
+    }
 </style>
