401a99ad9b679f5252365ad7afe1c62a918415a1
DanielMarella <marella.daniel96@gmail.com>
2023-06-16 17:49:25 +0200

    create another components for visualization of the cards

---
+++ src/components/MainCard.vue
@@ -0,0 +1,24 @@
+
+<template>
+    <div>
+        <img :src="cardElement.thumb" alt="">
+        <p>
+            {{ cardElement.series }}
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name : 'Card',
+
+    props : [
+        'cardElement'
+    ]
+}
+</script>
+
+
+<style lang="scss" scoped>
+    
+</style>
