5aadc42effc025f8491ebf975c4a075736057c23
DanielMarella <marella.daniel96@gmail.com>
2023-06-16 18:06:41 +0200

    add all the cards and fix some style

--- src/components/MainApp.vue
+++ src/components/MainApp.vue
@@ -4,7 +4,11 @@
             <div class="content">
                 <MainCard v-for="card in products" 
                 :cardElement = "card" />
+                <button>LOAD MORE</button>
             </div>
+            <div class="button">
+            </div>
+
             <div class="main-card">
 
             </div>
@@ -24,14 +28,79 @@ export default {
     data() {
         return {
             products : [
-                {
-                    "thumb": "https://www.coverbrowser.com/image/action-comics/1-1.jpg",
-                    "price": "$19.99",
-                    "series": "Action Comics",
-                    "type": "comic book",
-                },
-                
-            ]
+                        {
+                            "thumb": "https://www.coverbrowser.com/image/action-comics/1-1.jpg",
+                            "price": "$19.99",
+                            "series": "Action Comics",
+                            "type": "comic book",
+                        },
+                        {
+                            "thumb": "https://www.panini.it/media/catalog/product/cache/a5b5dd3adfe0d321084804c738f29601/M/1/M1BLLA015ISBN_0.jpg",
+                            "price": "$3.99",
+                            "series": "American Vampire 1976",
+                            "type": "comic book",
+                        },
+                        {
+                            "thumb": "https://media.wired.com/photos/593384dad80dd005b42b2817/master/w_2560%2Cc_limit/Aquaman-16.jpg",
+                            "price": "$16.99",
+                            "series": "Aquaman",
+                            "type": "graphic novel",
+                        },
+                        {
+                            "thumb": "https://d29xot63vimef3.cloudfront.net/image/batgirl/1-1.jpg",
+                            "price": "$2.99",
+                            "series": "Batgirl",
+                            "type": "comic book",
+                        },
+                        {
+                            "thumb": "https://static.posters.cz/image/750/locandine-film-in-plexiglass-batman-prowl-comic-cover-i69653.jpg",
+                            "price": "$3.99",
+                            "series": "Batman",
+                            "type": "comic book",
+                        },
+                        {
+                            "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/5/50/Batman_Beyond_v.1_1.jpg",
+                            "price": "$2.99",
+                            "series": "Batman Beyond",
+                            "type": "comic book",
+                        },
+                        {
+                            "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/0/0d/Batman_Superman_Vol_1_1.jpg",
+                            "price": "$3.99",
+                            "series": "Batman/Superman",
+                            "type": "comic book",
+                        },
+                        {
+                            "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/c/cf/Batman_Superman_Annual_Vol_2_1.jpg",
+                            "price": "$4.99",
+                            "series": "Batman/Superman Annual",
+                            "type": "comic book",
+                        },
+                        {
+                            "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/5/54/Batman_The_Joker_War_Zone_Vol_1_1.jpg",
+                            "price": "$5.99",
+                            "series": "Batman: The Joker War Zone",
+                            "type": "comic book",
+                        },
+                        {
+                            "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/6/64/Batman_Three_Jokers_Collected.jpg",
+                            "price": "$6.99",
+                            "series": "Batman: Three Jokers",
+                            "type": "comic book",
+                        },
+                        {
+                            "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/f/f8/Batman_White_Knight_Presents_Harley_Quinn_Vol_1_1.jpg",
+                            "price": "$4.99",
+                            "series": "Batman: White Knight Presents: Harley Quinn",
+                            "type": "comic book",
+                        },
+                        {
+                            "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/c/c8/Catwoman_Vol_2_1.jpg",
+                            "price": "$16.99",
+                            "series": "Catwoman",
+                            "type": "graphic novel",
+                        },
+                    ]
         }
     },
 
@@ -49,12 +118,26 @@ export default {
     }
 
     .content{
-        margin: 0 150px;
+        width: 1000px;
+        margin: 0 auto;
+        display: flex;
+        flex-wrap: wrap;
     }
 
     .main-card{
         background-color: #467df5;
         height: 150px;
         width: 100%;
+    }
+
+    .button{
+    }
+
+    button{
+        margin: 10px auto 20px auto;
+        color: white;
+        background-color: #467df5;
+        border: none;
+        padding: 20px;
     }
 </style>
