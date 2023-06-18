919077b3b9eea5c850f5f6b1366c936b36cabc9c
DanielMarella <marella.daniel96@gmail.com>
2023-06-18 13:56:47 +0200

    creating new component call JumboTron, add the img dinamically and some style

---
+++ src/components/JumboTron.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="jumbo">
+        <img :src="getImagePath(jumboImg)" alt="">
+    </div>
+    <div class="current-series">
+        <p>current series</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name : 'JumboTron',    
+    data() {
+        return {
+            jumboImg : "jumbotron.jpg",
+
+
+        }
+    },
+    
+                methods : { 
+                    getImagePath : function(img) {
+                    return new URL (`../assets/img/${img}`, import.meta.url).href;
+                    }
+                }
+}
+
+</script>
+
+<style lang="scss" scoped>
+    .jumbo{
+        position: relative;
+        width: 100%;
+        height: 400px;
+        
+            img{
+                width: 100%;
+                height: 100%;
+                object-fit: cover;
+                object-position: top;
+            }
+
+        }
+    .current-series{
+
+        p{ 
+            padding: 20px 50px;
+            position: absolute;
+            top: 500px;
+            left: 350px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            color: white;
+            text-transform: uppercase;
+            background-color: #467df5;
+        }
+    }        
+</style>
